e074965273e6dc164ca588a38930c41aada121cb
ssayno <418215971@qq.com>
2021-10-23 20:20:36 +0800

    test

--- init.el
+++ init.el
@@ -129,7 +129,20 @@
               100)
         '(85 . 50) '(100 . 100)))))
 (global-set-key (kbd "C-c t") 'toggle-transparency)
+;; open the 'ido-mode
+(require 'ido)
+(ido-mode 1)
+(setf (nth 2 ido-decorations) "\n")
+(require 'icomplete)
+(icomplete-mode 1)
+(setq icomplete-separator "\n")
+(setq icomplete-hide-common-prefix nil)
+(setq icomplete-in-buffer t)
+
 ;;; my basic config
+
+
+
 (require 'mylatex)
 (require 'mymarkdown)
 (require 'myelisp)
@@ -143,7 +156,7 @@
  '(custom-safe-themes
    '("830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" default))
  '(package-selected-packages
-   '(web-mode use-package solarized-theme rainbow-delimiters prettier-js pdf-tools neotree markdown-preview-eww markdown-mode magit lispy js2-mode highlight-parentheses flycheck flucui-themes flex-autopair fcitx exec-path-from-shell emmet-mode edit-indirect color-theme-sanityinc-tomorrow cdlatex auto-complete-auctex auctex async)))
+   '(use-package solarized-theme rainbow-delimiters pdf-tools neotree markdown-preview-eww markdown-mode magit lispy highlight-parentheses flycheck flex-autopair exec-path-from-shell edit-indirect cdlatex auto-complete-auctex auctex async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
